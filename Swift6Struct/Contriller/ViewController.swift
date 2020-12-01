a703f125cd775665fbd811531b538d40c2b53253
Yoshiki <villager2525@gmail.com>
2020-12-01 23:09:41 +0900

    first commit

---
+++ Swift6Struct/Contriller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift6Struct
+//
+//  Created by 西谷恭紀 on 2020/11/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
