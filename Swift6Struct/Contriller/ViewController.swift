873a133689fadf24690cfb6af6a2746fb974f74e
Yoshiki <villager2525@gmail.com>
2020-12-01 23:25:51 +0900

    セルの構築

--- Swift6Struct/Contriller/ViewController.swift
+++ Swift6Struct/Contriller/ViewController.swift
@@ -7,13 +7,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    
+
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+     
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let userNameLabel = cell.contentView.viewWithTag(1)
+        
+        let shumiLabel = cell.contentView.viewWithTag(2)
+        
+        let movieLabel = cell.contentView.viewWithTag(3)
+        
+        return cell
+        
+    }
 
 }
 
