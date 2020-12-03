3d9207e3937d744eb2c0abf137be4ef3efbb2545
Yoshiki <villager2525@gmail.com>
2020-12-03 23:42:02 +0900

    モデル追加

--- Swift6Struct/Controller/NextViewController.swift
+++ Swift6Struct/Controller/NextViewController.swift
@@ -7,15 +7,34 @@
 
 import UIKit
 
+protocol SetOkDelegate {
+    func setOk(check:[Person])
+}
+
 class NextViewController: UIViewController {
 
+    //インスタンス
+    var person = Person()
+    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var shumiTextField: UITextField!
+    @IBOutlet var movieTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        person.name = nameTextField.text!
+        person.shumi = shumiTextField.text!
+        person.movie = movieTextField.text!
+        
+        dismiss(animated: true, completion: nil)
+
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func done(_ sender: Any) {
+    }
+    
     
     
 
---
+++ Swift6Struct/Model/Person.swift
@@ -0,0 +1,16 @@
+//
+//  Person.swift
+//  Swift6Struct
+//
+//  Created by 西谷恭紀 on 2020/12/03.
+//
+
+import Foundation
+
+struct Person {
+    
+    var name = String()
+    var shumi = String()
+    var movie = String()
+    
+}
